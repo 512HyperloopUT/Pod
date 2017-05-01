bdf9a6f957a6ba5978200d5ecf6efa576e7dcc2f
Benajmin Xu <ben.benjamin.ben@gmail.com>
2017-04-30 18:53:43 -0700

    Pin mappings modified, TM4C code refined slightly

--- TM4C/HyperloopTM4C/main.c
+++ TM4C/HyperloopTM4C/main.c
@@ -1,17 +1,33 @@
+/* C libraries */
 #include <stdint.h>
+#include <stdbool.h>
+
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "driverlib/gpio.h"
+#include "inc/tm4c1294ncpdt.h"
+
+#include "utils/uartstdio.h"
+
 #include "drivers/pinout.h"
+
+#include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/rom.h"
 #include "driverlib/rom_map.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
-#include "utils/uartstdio.h"
-#include "inc/tm4c1294ncpdt.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/systick.h"
 
-#define PIDEBUG0
+/* Run constants */
+/* Analog read is needed */
+#define ADC_REQ 0
+/* Analog write is needed */
+#define PWM_REQ
+/* Only stt and cmd IO ports */
+#define PI_SIMPLE 1
+/* All 4 ports needed */
+#define PI_ADVANCED 0
 
 /*****************
 The error routine that is called if the driver library encounters an error
@@ -23,22 +39,51 @@ __error__(char *pcFilename, uint32_t ui32Line) {
 #endif
 
 /*****************
-Pin Data Fetch
+Configure SysTick for use with timing data
 *****************/
+uint64_t curr_time = 0;
+void SysTick_IntHandler(void) { curr_time++; }
+void ConfigureTiming(void) {
+    /* Settle clock */
+    uint32_t clock = SysCtlClockGet();
+    /* Make a function available for handling the SysTick */
+    SysTickIntRegister(SysTick_IntHandler);
+    /* Equivalent Clock speed and SysTick */
+    SysTickPeriodSet(SysCtlClockGet());
+    /* Enable interrupts */
+    IntMasterEnable();
+    /* Enables SysTick interrupts */
+    SysTickIntEnable();
+    /* Enable the SysTick itself */
+    SysTickEnable();
+}
+
+/*****************
+Pin Mapping Macros
+*****************/
+#define SYSPORT(A) SYSCTL_PERIPH_GPIO ## A
 #define PORT(A) GPIO_PORT ## A ## _BASE
 #define PIN(A) GPIO_PIN_ ## A
-//define PIN(A) (1 << A)
 #define PP(A, B) PORT(A), PIN(B)
 /*****************
-Pin Init
+Pin GPIO Init Macros and Functions
 *****************/
 #define PIN_IN 1
 #define PIN_OUT 0
-#define PTO ROM_GPIOPinTypeGPIOOutput
-#define PTI ROM_GPIOPinTypeGPIOInput
+#define PTO GPIOPinTypeGPIOOutput
+#define PTI GPIOPinTypeGPIOInput
 #define GPIOPinInit(port, pin, type) type ? PTI(port, pin) : PTO(port, pin)
+/* Enable the ports for GPIO */
+void GPIOPeriphInit(uint32_t* ports, uint8_t len) {
+    int i;
+    for (i = 0; i < len; ++i) {
+        SysCtlPeriphEnable(ports[i]);
+        while (!SysCtlPeriphReady(ports[i]));
+    }
+}
 void GPIOMassInit(uint32_t *ports, uint8_t *pins, uint8_t len, uint8_t input) {
-    for (int i = 0; i < len; ++i) {
+    int i;
+    for (i = 0; i < len; ++i) {
         GPIOPinInit(ports[i], pins[i], input);
     }
 }
@@ -51,25 +96,26 @@ Digital Pin IO
 #define DigiReadPin GPIOPinRead
 /* Only up to 64 bits, read starts reading to bit 0 */
 uint64_t MassRead(uint32_t *ports, uint8_t *pins, uint8_t len) {
+    int i;
     uint64_t val = 0;
-    for (int i = 0; i < len; ++i) {
-        val |= DigiReadPin(ports[i], pins[i]) << i;
+    for (i = 0; i < len; ++i) {
+        val |= (DigiReadPin(ports[i], pins[i]) ? 1 : 0) << i;
     }
     return val;
 }
 /* Only up to 64 bits, write starting with bit 0 */
 void MassWrite(uint32_t *ports, uint8_t *pins, uint8_t len, uint64_t data) {
-    for (int i = 0; i < len; ++i) {
-        curr_vals << 1;
-        DigiWritePin(ports[i], pins[i], (data >> i) & 1);
+    int i;
+    for (i = 0; i < len; ++i) {
+        DigiWritePin(ports[i], pins[i], ((data >> i) & 1) ? -1 : 0);
     }
 }
 
 /*****************
 Analog Pin IO
 *****************/
-#ifndef PIDEBUG0
-    #error TODO
+#if ADC_REQ == 1 || PWM_REQ == 1
+    #error "Analog writes on the TODO list"
 #endif
 
 /*****************
@@ -77,100 +123,50 @@ Configure communication with the Raspberry Pi, pin mappings and method.
 *****************/
 /*****************
 PIN MAPPINGS (LSb -> MSb)
-
-PWM OUT
-PM3
-PF1
-PF2
-PF3
-PG0
-PM7
-PG1
-PK4
-PK5
-PM0
-
-BRAKE OUT
-PC6
-
-TIMER CAPTURE
-PM1
-PM2
-
-
-PC7
-PH2
-PH3
-PN2
-PN3
-PP2
-
-
-ANALOG IN
-???? -> PA6
-AIN0 -> PE3
-AIN1 -> PE2
-AIN2 -> PE1
-AIN3 -> PE0
-AIN4 -> PD7
-
-
-AIN8 -> PE5
-AIN9 -> PE4
-AIN13 -> PD2
-AIN10 -> PB4
-AIN11 -> PB5
-AIN16 -> 
-AIN17 -> 
-AIN18 -> 
-AIN19 -> 
-
-
-output:
-    stt
-        PA7
-    odata
-        PM6, PM7, PN2, PN3,
-        PP2, PP3, PP4, PP5,
-        PQ1, PL3, PL4, PL5
-input:
-    cmd PM0, PM1, PM2, PM3
-    idata
-        PC7, PF1, PF2, PF3,
-        PG0, PH0, PH1, PH2,
-        PH3, PK4, PK5, PK6
+stt
+    PA7
+odata
+    PL1, PL2, PL3, PL4,
+    PL5, PM0, PM1, PM2,
+    PM3, PM6, PM7, PN2
+cmd
+    PK5, PK6, PK7, PL0
+idata
+    PC6, PC7, PN3, PF2,
+    PF3, PG0, PG1, PH0,
+    PH1, PH2, PH3, PK4
 *****************/
 uint8_t stt_width = 1;
 uint32_t stt_ports[] = {PORT(A)};
 uint8_t stt_pins[] = {PIN(7)};
 uint8_t odata_ports_width = 12;
 uint32_t odata_ports[] = {
-    {PORT(M)}, {PORT(M)}, {PORT(N)}, {PORT(N)},
-    {PORT(P)}, {PORT(P)}, {PORT(P)}, {PORT(P)},
-    {PORT(Q)}, {PORT(L)}, {PORT(L)}, {PORT(L)}
+    PORT(L), PORT(L), PORT(L), PORT(L),
+    PORT(L), PORT(M), PORT(M), PORT(M),
+    PORT(M), PORT(M), PORT(M), PORT(N)
 };
 uint8_t odata_pins[] = {
-    {PIN(6)}, {PIN(7)}, {PIN(2)}, {PIN(3)},
-    {PIN(2)}, {PIN(3)}, {PIN(4)}, {PIN(5)},
-    {PIN(1)}, {PIN(3)}, {PIN(4)}, {PIN(5)}
+    PIN(1), PIN(2), PIN(3), PIN(4),
+    PIN(5), PIN(0), PIN(1), PIN(2),
+    PIN(3), PIN(6), PIN(7), PIN(2)
 };
 uint8_t cmd_width = 4;
 uint32_t cmd_ports[] = {
-    {PORT(M)}, {PORT(M)}, {PORT(M)}, {PORT(M)}
+    PORT(K), PORT(K), PORT(K), PORT(L)
 };
 uint8_t cmd_pins[] = {
-    {PIN(0)}, {PIN(1)}, {PIN(2)}, {PIN(3)}
+    PIN(5), PIN(6), PIN(7), PIN(0)
 };
 uint8_t idata_width = 12;
 uint32_t idata_ports[] = {
-    {PORT(C)}, {PORT(F)}, {PORT(F)}, {PORT(F)},
-    {PORT(G)}, {PORT(H)}, {PORT(H)}, {PORT(H)},
-    {PORT(H)}, {PORT(K)}, {PORT(K)}, {PORT(K)}
+    PORT(C), PORT(C), PORT(N), PORT(F),
+    PORT(F), PORT(G), PORT(G), PORT(H),
+    PORT(H), PORT(H), PORT(H), PORT(H)
 };
 uint8_t idata_pins[] = {
-    {PIN(7)}, {PIN(1)}, {PIN(2)}, {PIN(3)},
-    {PIN(0)}, {PIN(0)}, {PIN(1)}, {PIN(2)},
-    {PIN(3)}, {PIN(4)}, {PIN(5)}, {PIN(6)}
+    PIN(6), PIN(7), PIN(3), PIN(2),
+    PIN(3), PIN(0), PIN(1), PIN(0),
+    PIN(1), PIN(2), PIN(3), PIN(4)
 };
 void ConfigurePiComm(void) {
     /* cmd */
@@ -186,12 +182,65 @@ void ConfigurePiComm(void) {
 /*****************
 Configure output
 *****************/
-uint32_t tx_ports[] = {};
-uint32_t tx_pins[] = {};
-uint32_t rx_ports[] = {};
-uint32_t rx_pins[] = {};
+/*****************
+Mappings to other things
+tx
+    PF1
+tx_targ
+    PP2, PP3, PP5, PQ1
+rx
+    PA6 -> ????, PB4 -> AIN10,
+    PB5 -> AIN11, PD2 -> AIN13,
+    PD7 -> AIN4, PE0 -> AIN3,
+    PE1 -> AIN2, PE2 -> AIN1,
+    PE3 -> AIN0, PE4 -> AIN9,
+    PE5 -> AIN8, PK0 -> AIN16,
+    PK1 -> AIN17, PK2 -> AIN18,
+    PK3 -> AIN19
+*****************/
+uint8_t tx_width = 1;
+uint32_t tx_ports[] = {
+    
+};
+uint8_t tx_pins[] = {
+    
+};
+uint8_t tx_targ_width = 4;
+uint32_t tx_targ_ports[] = {
+    
+};
+uint8_t tx__targ_pins[] = {
+    
+};
+uint8_t rx_width = 0;
+uint32_t rx_ports[] = {
+    
+};
+uint8_t rx_pins[] = {
+    
+};
 void ConfigureOuput(void) {
     /* Analog outputs */
+    #if ANALOG_REQ == 1
+        #error "Analog output must be configured."
+    #endif
+    #if ADC_REQ == 1
+        #error "Analog read must be configured."
+    #endif
+}
+
+/*****************
+Configure GPIO peripherals related to the above issues
+*****************/
+uint8_t pi_port_count = 11;
+uint32_t pi_comm_ports[] = {
+    SYSPORT(A), SYSPORT(C), SYSPORT(F), SYSPORT(G),
+    SYSPORT(H), SYSPORT(K), SYSPORT(L), SYSPORT(M),
+    SYSPORT(N)
+};
+void ConfigurePeripherals(void) {
+    /* Setup peripherals */
+    GPIOPeriphInit(pi_comm_ports, pi_port_count);
 }
 
 /*****************
@@ -209,22 +258,6 @@ void ConfigureUART(void) {
     ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
     /* Initialize the UART for console I/O */
     UARTStdioConfig(0, 115200, g_ui32SysClock);
-}
-
-/*****************
-Attempt to use SysTick for timing data
-*****************/
-uint64_t curr_time = 0;
-void SysTick_IntHandler(void) {
-    curr_time++;
-}
-void ConfigureTiming(void) { //Try for 1ms ticks
-    uint32_t clock = SysCtlClockGet();
-    SysTickIntRegister(SysTick_IntHandler);
-    SysTickPeriodSet(SysCtlClockGet());
-    IntMasterEnable();
-    SysTickIntEnable();
-    SysTickEnable();
 }
 
 /*****************
@@ -269,51 +302,47 @@ void ReadCmd(void) {
 uint64_t last_exec = 0;
 uint16_t exec_sep = 25;
 void ExecCmd(void) {
-    #ifndef PIDEBUG0
-        #error TODO
+#if PI_ADVANCED == 1
     if (
             (curr_cmd != last_cmd || curr_idata != last_idata)
             || (curr_time - last_exec >= exec_sep || exec_beat == 0)
     ) {
         /* Set time data */
-        #error TODO
-        last_exec = /* current time */;
+        last_exec = curr_time;
         /* "Lock" pins */
         WritePin(stt_ports[0], stt_pins[0], PIN_OFF);
         switch (curr_cmd) {
         case 0:
-            /* Full reset */
-            curr_odata = 0;
-            curr_stt = 0;
-            break;
-        case 1: /* Full instruction */
-            #ifndef PIDEBUG0
-            if (curr_idata < 0x8000) { /* Simple value fetch */
-                #ifndef PIDEBUG0
-                    #error TODO
+        case 1: /* Read instruction */
+            if (curr_idata < 0x800) { /* Simple value fetch */
+                #if ADC_REQ == 1
+                    #error "Must initialize and configure ADC"
                 #endif
             } else { /* Preset instruction */
                 CustomFunct(curr_idata);
             }
-            #endif
+            curr_stt = curr_cmd;
             break;
         default: /* Writing to motors */
-            #ifndef PIDEBUG0
-                #error TODO
+            #if PWM_REQ == 1
+                #error "ADC Not Setup"
             #endif
         }
         /* Read necessary data */
-        #error TODO
-        curr_odata = /* ????? */;
+        #if ADC_REQ == 1
+            #error "ADC not set up"
+        #endif
+        curr_odata = ReadAnalogPin(/* Decoded pin here */);
         /* Write valp */
         MassWrite(odata_ports, odata_pins, odata_width, curr_odata);
         /* Write s */
         WritePin(stt_ports[0], stt_pins[0], curr_stt);
     }
-    
-    #endif
-    
-    MassWrite(stt_ports, stt_pins, stt_width, !cmd);
+#elif PI_SIMPLE == 1
+    MassWrite(stt_ports, stt_pins, stt_width, cmd ? 0 : 1);
+#else
+    #error "No Pi communication protocol established"
+#endif
 }
 
 /*****************
@@ -332,7 +361,7 @@ void Heartbeat(void) {
 }
 
 /*****************
-Generic loop function
+Generic loop function and main function
 *****************/
 inline void Loop(void) {
     ReadCmd();
