c2f6b8798db11397a452170d515e3140ddacb6f0
davidgyyuan <davidyuan@utexas.edu>
2017-08-06 16:50:23 -0500

    localhost sockets

--- Pi/cli.py
+++ Pi/cli.py
@@ -1,45 +1,74 @@
 import random
-import time, struct
+import time
+import struct
 from socket import socket, AF_INET, SOCK_DGRAM, gethostbyname
 import threading
 
-SERVER_IP = raw_input("Server IP: ")
+isDebug = True
+
+SERVER_IP = '192.168.0.' + raw_input("Server IP: ")
 PORT_NUMBER = 3000
+SEND_PORT = ('localhost', 8889)
 SIZE = 1024
 
 mySocket = socket(AF_INET, SOCK_DGRAM)
-#mySocket.connect((SERVER_IP, PORT_NUMBER))
+# mySocket.connect((SERVER_IP, PORT_NUMBER))
+sendSocket = socket(AF_INET, SOCK_DGRAM)
 hostName = gethostbyname('0.0.0.0')
 mySocket.bind((hostName, PORT_NUMBER))
+inputData = []
 
 def fillData():
     """
     :return: A string with the encoded data received from pod.
     """
-    n = 10
-    inputdata = ()
-    for i in range(n):
-        inputdata += (int(random.random()*10),)
-    for i in range(10 - len(inputdata)):
-        inputdata += (0,)
+    global inputData
+
+    if isDebug:
+        for _ in 8:
+            inputData.append(int(random.random() * 10))
+            time.sleep(.5)
+    for i in range(10 - len(inputData)):
+        inputData.append(0)
     packer = struct.Struct('! B B i i i i i i i I')
-    packeddata = packer.pack(*inputdata)
+    packeddata = packer.pack(*inputData)
     return packeddata
 
 def checkButton():
     """
-    Continously checks for signal that emergency stop has activated.
+    Continously checks for...
+        - Signal that emergency.
+        - A time update.
+        - New output data from localhost
     """
+    global inputData
     while True:
-        (load, addr) = mySocket.recvfrom(35)
-        if load == 's'*35:
-            print "EMERGENCY STOP"
+        (load, addr) = mySocket.recvfrom(SIZE)
+        seconds = load.split()
+        if load.startswith('s'*35):
+            if len(seconds) == 1:
+                if isDebug:
+                    print "EMERGENCY STOP"
+                else:
+                    sendSocket.sendto('-1', SEND_PORT)
+            else:
+                if isDebug:
+                    print seconds
+                else:
+                    sendSocket.sendto(seconds[1], SEND_PORT)
+        else:
+            inputData = seconds
+
 
 threading.Thread(target=checkButton).start()
+prevData = [0] * 10
 while True:
     data = fillData()
-    try:
-        mySocket.sendto(data, (SERVER_IP, PORT_NUMBER))
-    except:
-        pass
-    time.sleep(.2)
+    if not data == prevData:
+        try:
+            mySocket.sendto(data, (SERVER_IP, PORT_NUMBER))
+            prevData = data
+        except:
+            # Not a big deal just try again...
+            time.sleep(.5)
+
