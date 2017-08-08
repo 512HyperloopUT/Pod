04f151d1ff81663e6a3f8a695b2fe559802da5d6
Noviv <matthewwebb65@yahoo.com>
2017-08-07 21:54:41 -0500

    fixed < 0

--- Pi/src/cpp/gpio/GPIOPin.cpp
+++ Pi/src/cpp/gpio/GPIOPin.cpp
@@ -75,7 +75,7 @@ bool GPIOPin::setDirection_s(GPIODir dir) {
 	char str[50];
 	DIR(str, port);
 	std::ofstream dirstream(str, std::ios::out | std::ios::trunc);
-	if (dirstream < 0) {
+	if (!dirstream.is_open()) {
 		printf("Could not set direction of GPIO pin %i\n", port);
 		return true;
 	}
@@ -95,7 +95,7 @@ bool GPIOPin::setValue_s(bool val) {
 	char str[50];
 	VAL(str, port);
 	std::ofstream valstream(str, std::ios::out | std::ios::trunc);
-	if (valstream < 0) {
+	if (!valstream.is_open()) {
 		printf("Could not set value of GPIO pin %i\n", port);
 		return true;
 	}
@@ -113,7 +113,7 @@ bool GPIOPin::getValue_s(bool& val) {
 	char str[50];
 	VAL(str, port);
 	std::ifstream valstream(str, std::ios::in);
-	if (valstream < 0) {
+	if (!valstream.is_open()) {
 		printf("Could not read value of GPIO pin %i\n", port);
 		return true;
 	}
