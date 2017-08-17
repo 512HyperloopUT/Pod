cde6c6ade83292ef6c7e004dea4c7c2f0fca933a
AlterionX <ben.benjamin.ben@gmail.com>
2017-08-16 22:52:18 -0400

    temporary braking update

--- Pi/central/pyvers/s_logger.py
+++ Pi/central/pyvers/s_logger.py
@@ -3,15 +3,14 @@
 
 # set up i2c on the pi
 # first, un-blacklist i2c
-# next, add the i2c modules to the thing
+# next, add the i2c modules to the loaded kernel modules
 # afterthat, install i2c-tools and reboot
 # finally, run i2cdetect
 
 # install smbus2
 
 import time
-
-import smbus2
+import Rpi.GPIO
 
 import pod_periph
 
@@ -22,7 +21,7 @@ class IMUSensor(pod_periph.I2CSensor):
         # TODO Analyze data
 
 
-BRAKE_INTERVAL = 10 # Change to change num. of seconds before braking
+BRAKE_INTERVAL = 10  # Change to change num. of seconds before braking
 sensor_ports = [
     IMUSensor("imu", 1, 0x28),
     pod_periph.AnalogSensor("prox0", 0),
@@ -32,6 +31,8 @@ sensor_ports = [
 ]
 cli = pod_periph.Client()
 
+RELAY_PIN_NUM = 10
+potentiometer = pod_periph.AnalogSensor("emergencypotent", 4)
 
 def update_sensors():
     for sensor in sensor_ports:
@@ -46,7 +47,17 @@ def log(outfile):
 if __name__ == "__main__":
     logfile = open("temp_log.txt")
     start = time.time()
+    Rpi.GPIO.setup(RELAY_PIN_NUM, Rpi.GPIO.OUT, initial=Rpi.GPIO.LOW)
     while time.time() - start < BRAKE_INTERVAL:
         update_sensors()
         log(logfile)
+
     # TODO Add in the braking
+    # TODO CHECK IF THIS WORKS
+    Rpi.GPIO.output(RELAY_PIN_NUM, Rpi.GPIO.HIGH)
+    potentiometer.update_sensor()
+
+    while potentiometer.value > SOME_MEANINGFUL_VALUE:
+        potentiometer.update_sensor(cli)
+
+    Rpi.GPIO.output(RELAY_PIN_NUM, Rpi.GPIO.HIGH)
