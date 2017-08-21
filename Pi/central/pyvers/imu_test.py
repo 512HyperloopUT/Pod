8ef05194b4d8be2b2fc544ca0ca34ed65aac905f
AlterionX <ben.benjamin.ben@gmail.com>
2017-08-21 02:55:13 -0400

    im dum

--- Pi/central/pyvers/imu_test.py
+++ Pi/central/pyvers/imu_test.py
@@ -8,9 +8,10 @@ ADDR = 0x28
 
 if __name__ == "__main__":
     imu = pod_periph.IMUSensor("imu", ADDR)
+    cli = pod_periph.Client()
     while True:
         print("Updating sensor. Values:")
-        imu.update_sensor(None)
+        imu.update_sensor(cli)
         print("\tdistance, magnitude: " + imu.values[0])
         print("\tvelocity: " + imu.values[1])
         print("\tcurrent orientation: " + imu.values[2])
