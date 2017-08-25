f23c18511319e0894d0d02c32214dc66ad206e9f
Noviv <matthewwebb65@yahoo.com>
2017-08-24 22:34:01 -0700

    removed IMU sensor

--- Pi/hyper/io.py
+++ Pi/hyper/io.py
@@ -11,7 +11,6 @@ class Input:
     def __init__(self):
         # input backends
         self.comm_port = comms.CommPort()
-        #self.imu = IMUSensor()
         self.udp_port = udp.UDPClient()
 
         self.voltmeter = AnalogSensor(18, self.comm_port)
@@ -53,15 +52,6 @@ class Input:
         self.pod_temperature = 9  # tenths of C
         self.stripe_count = 10
 
-        self.accelerationX = 0
-        self.accelerationY = 0
-        self.accelerationZ = 0
-
-        self.OriW = 0
-        self.OriX = 0
-        self.OriY = 0
-        self.OriZ = 0
-
         self.voltage = 0
         self.amperage_highcurrent = 0
         self.amperage_lowcurrent = 0
@@ -78,14 +68,6 @@ class Input:
 
     def update(self):
         self.duration = int(time.time() * 1000) - self.start_time
-        #self.accelerationX = self.imu.getAccX()
-        #self.accelerationY = self.imu.getAccY()
-        #self.accelerationZ = self.imu.getAccZ()
-
-        #self.OriW = self.imu.getOriW()
-        #self.OriX = self.imu.getOriX()
-        #self.OriY = self.imu.getAccY()
-        #self.OriZ = self.imu.getOriZ()
 
         self.udp_port.update()
         self.emag_activated = self.udp_port.ebrake
@@ -108,7 +90,7 @@ class Input:
 
         # processing of data to be sent to GUI and SpaceX TODO: add other variable
         self.battery_voltage = self.voltage * 1000
-        self.battery_current = self.battery_voltage * 1000
+        self.battery_current = self.amperage_highcurrent * 1000
 
     def get_packed_data(self):
         """
@@ -181,94 +163,6 @@ class Voltmeter:
 
     def get(self):
         return self.value
-
-
-class IMUSensor:
-    IMU_ACCEL_SMOOTHING = 10
-    IMU_ORIEN_SMOOTHING = 0.7
-
-    def __init__(self):
-        self.bno = bno055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
-        if self.bno.begin() is not True:
-            print("Error initializing device")
-            exit()
-        time.sleep(1)
-        # TODO not sure about this line
-        # self.bno.setExternalCrystalUse(True)
-
-        self.initial_rot = quat.normalize(self.bno.read_quaternion())
-        self.value = [(0.0, 0.0, 0.0), (0.0, 0.0, 0.0),
-                      self.initial_rot, (0.0, 0.0, 0.0), time.time()]
-
-    def update(self):
-        self.value = self.value[0: 2] + [self.bno.read_quaternion(), self.bno.read_linear_acceleration(),
-                                         time.time()] + self.value[2: 5]
-        # self.value[3] = tuple([round(x, 1) for x in self.value[3]])
-
-        delta_t = self.value[4] - self.value[7]
-
-        # simple complimentary lowpass on orientation
-        # TODO test efficacy
-        self.value[2] = tuple([
-            (prev * IMUSensor.IMU_ORIEN_SMOOTHING) +
-            (curr * (1 - IMUSensor.IMU_ORIEN_SMOOTHING))
-            for prev, curr in zip(self.value[5], self.value[2])
-        ])
-        # get the rotation from current coordinate system to original coordinate system
-        rot_q = quat.q_mult(quat.q_conjugate(
-            quat.normalize(self.value[2])), self.initial_rot)
-        # Rotate relative, linear acceleration to initial orientation
-        self.value[3] = quat.qv_mult(rot_q, self.value[3])
-        # Do a lowpass on accel
-        # TODO test efficacy
-        self.value[3] = tuple([
-            prev + (delta_t * (curr - prev) / IMUSensor.IMU_ACCEL_SMOOTHING)
-            for prev, curr in zip(self.value[6], self.value[3])
-        ])
-        # TODO add in a filter here for accuracy sake
-        # Add accel to velocity to get the current velocity
-        self.value[1] = (
-            self.value[1][0] + self.value[3][0],
-            self.value[1][1] + self.value[3][1],
-            self.value[1][2] + self.value[3][2]
-        )
-        # Do a simple time delta for the new distance traveled
-        self.value[0] = (
-            self.value[0][0] + (self.value[1][0] *
-                                (self.value[4] - self.value[7])),
-            self.value[0][1] + (self.value[1][0] *
-                                (self.value[4] - self.value[7])),
-            self.value[0][2] + (self.value[1][0] *
-                                (self.value[4] - self.value[7]))
-        )
-
-    def data_string(self):
-        return "\n\torientation: " + str(self.value[0]) + \
-            "\n\tvelocity: " + str(self.value[1]) + \
-            "\n\tcurrent orientation: " + str(self.value[2]) + \
-            "\n\tcurrent accel: " + str(self.value[3]) + \
-            "\n\tcurrent time: " + str(self.value[4])
-
-    def getAccX(self):
-        return self.value[3][0]
-
-    def getAccY(self):
-        return self.value[3][1]
-
-    def getAccZ(self):
-        return self.value[3][2]
-
-    def getOriW(self):
-        return self.value[2][0]
-
-    def getOriX(self):
-        return self.value[2][1]
-
-    def getOriY(self):
-        return self.value[2][2]
-
-    def getOriZ(self):
-        return self.value[2][3]
 
 
 class ThermoSensor:
